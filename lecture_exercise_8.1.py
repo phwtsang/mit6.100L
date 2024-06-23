42da3fd9a9f93085310e77a6bfcd29404956729f
Pui Ho Wilson Tsang <wtsang@scusd.net>
2024-06-23 04:42:58 +0000

    first save

---
+++ lecture_exercise_8.1.py
@@ -0,0 +1,10 @@
+main():
+
+def count_nums_with_sqrt_close_to(n,epsilon):
+    """
+    n is an integer > 2
+    epsilon is a positive number < 1
+    returns how many integers have a square root within epsilon of n
+    """
+
+print('Hello')
